b53971ca9f192481901b669fdaad10514b1231d2
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-06 16:15:52 +0200

    Added admin user interface

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [
@@ -5,4 +6,5 @@ urlpatterns = [
 	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
+	url(r'^admin/', include(admin.site.urls)),
 ]
