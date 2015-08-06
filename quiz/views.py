8baeef5f7ab1ed9c0bede73843842be978eae71b
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-06 11:02:45 +0200

    Changed definition of start page in quiz/views.py and linked to new data in start page.html

--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,27 @@
+# coding: utf-8
+
 from django.shortcuts import render
 
-# coding: utf-8
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
 # Create your views here.
 def startpage(request):
-	return render(request, "quiz/startpage.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/startpage.html", context)
 def quiz(request):
 	return render(request, "quiz/monstermastaren.html")
 def question(request):
