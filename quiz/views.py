d1a4102050d192145f00555de11e72ff10c9b053
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-06 10:21:26 +0200

    Created four views in quiz/views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+	return render(request, "quiz/startpage.html")
+def quiz(request):
+	return render(request, "quiz/monstermastaren.html")
+def question(request):
+	return render(request, "quiz/monstermastaren1.html")
+def completed(request):
+	return render(request, "quiz/resultat_monstermastaren.html")
