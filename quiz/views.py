f93e988d384122f3f022d057f83cf810031f9cc2
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-07 09:56:21 +0200

    Imported Quiz and changed request in views.py, changed loop on startpage.html

--- quiz/views.py
+++ quiz/views.py
@@ -1,31 +1,12 @@
 # coding: utf-8
 
 from django.shortcuts import render
-
-quizzes = {
-	"monstermastaren": {
-   		"name": u"Mönstermästaren",
-	   	"description": u"Känn igen djuret på dess mönster."
-	},
-	"nosa": {
-	   	"name": u"Nosa rätt på djuret",
-	   	"description": u"Vem gömmer sig bakom nosen, näsan eller näbben?"
-	},
-	"tassar": {
-	    "name": u"Alla tassarna på jorden",
-	    "description": u"Kan du se vem fötterna tillhör?"	},
-	"raven": {
-	    "name": u"Vad säger räven?",
-	    "description": u"Lyssna på djurens läten och gissa vem som är vem."	},
-	"kvittkvitt": {
-	    "name": u"Kvitt-kvitt-kvivitt-kvitt",
-	    "description": u"Kan du känna igen fågelsången?"	},
-}
+from quiz.models import Quiz
 
 # Create your views here.
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+	    	"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/startpage.html", context)
 def quiz(request, slug):
