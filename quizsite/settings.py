865cbe2b093b06db2dfa6664dd0a28102505c216
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-07 14:49:27 +0200

    Changed database setting

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -74,14 +74,13 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
