208f3c2b93affe0ff7ccfdead760f5dd1b1195e6
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-08-07 13:58:26 +0200

    Added get result data + loop for correct answers in completed view in views.py

--- quiz/views.py
+++ quiz/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import redirect
 # Create your views here.
 def startpage(request):
 	context = {
-	    	"quizzes": Quiz.objects.all(),
+			"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/startpage.html", context)
 def quiz(request, slug):
@@ -21,36 +21,43 @@ def question(request, slug, number):
 	quiz = Quiz.objects.get(slug=slug)
 	questions = quiz.questions.all()
 	if request.POST:
-    	answer = int(request.POST["answer"])
+		answer = int(request.POST["answer"])
 
-    	saved_answers = {}
-	if quiz.slug in request.session:
-		saved_answers = request.session[quiz.slug]
+		saved_answers = {}
+		if quiz.slug in request.session:
+			saved_answers = request.session[quiz.slug]
 
-    	saved_answers[str(number)] = answer
-    	request.session[quiz.slug] = saved_answers
-
-    	if questions.count() == number:
-        		return redirect("completed_page", quiz.slug)
-    	else:
-       		return redirect("question_page", quiz.slug, number +1)
-
-	# if number > questions.count():
-	# 	return redirect("completed_page", quiz.slug)
+		saved_answers[str(number)] = answer
+		request.session[quiz.slug] = saved_answers
+		print number
+		print questions.count()
+		if questions.count() == number:
+			return redirect("completed_page", quiz.slug)
+		else:
+			return redirect("question_page", quiz.slug, number +1)
 	question = questions[number - 1]
 	context = {
 		"question_number": number,
 		"question": question.question,
 		"answer1": question.answer1,
-	   	"answer2": question.answer2,
-	    "answer3": question.answer3,
-	    "quiz": quiz,
+		"answer2": question.answer2,
+		"answer3": question.answer3,
+		"quiz": quiz,
 	}
 	return render(request, "quiz/monstermastaren1.html", context)
 def completed(request, slug):
+	quiz = Quiz.objects.get(slug=slug)
+	questions = quiz.questions.all()
+	saved_answers = request.session[slug]
+	num_correct_answers = 0
+	
+	for counter, question in enumerate(questions):
+		if question.correct == saved_answers[str(counter + 1)]:
+			num_correct_answers += 1
+
 	context = {
-		"correct": 12,
-		"total": 20,
-		"quiz_slug": slug,
+		"correct": num_correct_answers,
+		"total": questions.count(),
+		"quiz": quiz,
 		}
 	return render(request, "quiz/resultat.html", context)
